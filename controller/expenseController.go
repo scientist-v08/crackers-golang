1d8752cb4003ede57d1f3945889386f463099681
Manoj.V <venkimanoj2@gmail.com>
2026-09-19 12:34:15 +0530

    [Feature] Added go routines for optimization

--- controller/expenseController.go
+++ controller/expenseController.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"strings"
+	"sync"
 
 	"github.com/gin-gonic/gin"
 	"github.com/scientist-v08/crackers/initializers"
@@ -75,23 +76,40 @@ func CreateExpense(c *gin.Context) {
 
 func GetAllExpenses(c *gin.Context) {
 
-	// 1. Obtain all the results
+	// 1. Create variables for obtaining errors and for go routines
+	var allExpensesErr, totalErr error
+	var wg sync.WaitGroup
 	var resOfAllExpenses []model.Expense
-	if err := initializers.DB.Find(&resOfAllExpenses).Error; err != nil {
+	var totalAmount int64
+
+	wg.Add(2)
+
+	// 2. Obtain all the results
+	go func() {
+		defer wg.Done()
+		allExpensesErr = initializers.DB.Find(&resOfAllExpenses).Error
+	}()
+	
+	// 3. Finding the total budget
+	go func() {
+		defer wg.Done()
+		totalErr = initializers.DB.Model(&model.Expense{}).Select("COALESCE(SUM(amount), 0)").Scan(&totalAmount).Error
+	}()
+
+	// 4. Wait until both the go routines finish.
+	wg.Wait()
+	
+	// 5. Return all the results
+	if allExpensesErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
 			"error": "Failed To fetch expenses",
 		})
 		return
 	}
-
-	// 2. Finding the total budget
-	var totalAmount int64
-	if err := initializers.DB.Model(&model.Expense{}).Select("COALESCE(SUM(amount), 0)").Scan(&totalAmount).Error; err != nil {
+	if totalErr != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch total"})
-        return
+		return
 	}
-
-	// 3. Return all the results
 	c.JSON(http.StatusOK, gin.H{
 		"expenses": resOfAllExpenses,
 		"total": totalAmount,
